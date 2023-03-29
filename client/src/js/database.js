e770f56bdff32f7b55928991f4e8d584a1c027eb
swmcguire <swmcguire1@comcast.net>
2023-03-29 18:52:08 -0500

    value is still not working - tweaked

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,7 +27,7 @@ const store = tx.objectStore('jate');
 
 
 // ----------------- Use the .add() method on the store and pass in the content.
-const request = store.put ({content}); 
+const request = store.add({content}); 
 
 // ----------------- Get confirmation of the request.
 const result = await request;
